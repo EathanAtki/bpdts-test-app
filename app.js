24e8b4192d4b583e31f6fc93920b41226273f20b
Eathan <eathan.dev@gmail.com>
2022-02-21 18:21:12 +0000

    chore: setup

---
+++ app.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+
+const { API_PORT } = process.env
+const port = process.env.PORT || API_PORT;
+
+const usersRouter = require('./routes/users')
+app.use('/users', usersRouter)
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
---
+++ routes/users.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+const express = require('express')
+const router = express.Router()
+
+// Get
+router.get("/", async (req, res) => {
+});
+
+module.exports = router
